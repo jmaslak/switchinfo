23003fbb84240e871c9e4dbf272e63f29a61a085
Joel Maslak <jmaslak@antelope.net>
2012-04-13 20:39:17 -0600

    Add query to see active macs

---
+++ sql/queries/listActiveMacs.sql
@@ -0,0 +1,25 @@
+--
+-- COPYRIGHT (C) 2012, ANTELOPE ENTERPRISES AND JOEL C. MASLAK
+-- ALL RIGHTS RESERVED
+--
+
+--
+-- Lists active MAC addresses, sorted by switch pretty name and
+-- then port pretty name.  Filters out uplinks.
+--
+
+SELECT	S.descr,
+	SP.descr,
+	M.mac,
+	M.descr
+FROM	mac_history MH,
+	switch S,
+	switchport SP,
+	mac M
+WHERE	MH.mac_id = M.mac_id
+  AND	MH.switchport_id = SP.switchport_id
+  AND	SP.switch_id = S.switch_id
+  AND	NOW() BETWEEN MH.start_dt AND MH.end_dt
+  AND	SP.uplink = false
+ORDER BY S.descr, SP.descr, M.mac, M.descr;
+
