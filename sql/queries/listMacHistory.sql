5318e6d267d212ec3328b5f38f6563056dc07da4
Joel Maslak <jmaslak@antelope.net>
2012-10-24 21:57:53 -0600

    Query to get history of a MAC address

---
+++ sql/queries/listMacHistory.sql
@@ -0,0 +1,33 @@
+--
+-- COPYRIGHT (C) 2012, ANTELOPE ENTERPRISES AND JOEL C. MASLAK
+-- ALL RIGHTS RESERVED
+--
+
+--
+-- Lists active MAC addresses, sorted by switch pretty name and
+-- then port pretty name.  Filters out uplinks.
+--
+
+--
+-- Note that the MAC address is encoded in the where clause below.
+--
+
+SELECT	S.descr switch_descr,
+	SP.descr switchport_descr,
+	M.mac mac,
+	M.descr mac_descr,
+	MH.start_dt,
+	CASE	WHEN MH.end_dt = '9999-12-31' THEN NULL
+		ELSE MH.end_dt
+	END AS end_dt
+FROM	mac_history MH,
+	switch S,
+	switchport SP,
+	mac M
+WHERE	MH.mac_id = M.mac_id
+  AND	MH.switchport_id = SP.switchport_id
+  AND	SP.switch_id = S.switch_id
+  AND	SP.uplink = false
+  AND	M.mac = 'MAC HERE'
+ORDER BY MH.start_dt, S.descr, SP.descr, M.mac, M.descr;
+
